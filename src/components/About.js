cdf83016d3f6e9f24aeef6f310c6b00750808c55
isfopo <55164207+isfopo@users.noreply.github.com>
2020-08-04 22:30:43 -0400

    adds artist image

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,32 @@
 import React from 'react'
 
 import { Links } from './Links.js';
-import { ArtistImage } from './ArtistImage';
+import { Symbol } from './Symbol.js';
+import artistImage from "../images/artistImage.png";
 
 export const About = props => {
     return (
         <div className="About">
             <div className="symbols">
                 <img
-                    src={`../../public/icons/${props.iconColor}/symbols/sound.png`} />
+                    src={`./public/icons/${props.iconColor}/symbols/sound.png`} 
+                    alt="sound icon"    
+                />
                 <img 
-                    src={`../../public/icons/${props.iconColor}/symbols/shape.png`} />
+                    src={`./public/icons/${props.iconColor}/symbols/shape.png`} 
+                    alt="shape icon"  
+                />
                 <img 
-                    src={`../../public/icons/${props.iconColor}/symbols/place.png`} />
+                    src={`./public/icons/${props.iconColor}/symbols/place.png`} 
+                    alt="place icon"  
+                />
                 <img 
-                    src={`../../public/icons/${props.iconColor}/symbols/memory.png`} />
+                    src={`./public/icons/${props.iconColor}/symbols/memory.png`} 
+                    alt="memory icon"  
+                />
             </div>
             <img 
-                src="../images/artistImage.png" />
+                src={artistImage} />
         </div>
     )
 }
