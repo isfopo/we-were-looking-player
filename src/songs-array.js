a3cd11ee9821c4a32d3b8136fdeb3fe5fc1bb4ca
isfopo <55164207+isfopo@users.noreply.github.com>
2020-08-10 20:17:09 -0400

    completes songsArray

--- src/songs-array.js
+++ src/songs-array.js
@@ -4,7 +4,7 @@ export const songsArray = [
         fileName : "immortal-in-his-own-sense",
         duration: "244.47260770975055",
         backgroundColor : "#CACCFA",
-        releaseDate : 21697019954750,
+        releaseDate : 0,
         iconColor : "dark",
         imagePosition : {
             position: 'fixed',
@@ -14,11 +14,11 @@ export const songsArray = [
         }
     },
     {
-        name : "",
-        fileName : "immortal-in-his-own-sense",
-        duration: "244.47260770975055",
-        backgroundColor : "#CACCFA",
-        releaseDate : 21697019954750,
+        name : "he steps into a dark room",
+        fileName : "he-steps-into-a-dark-room",
+        duration: 0,
+        backgroundColor : "#B8CBEA",
+        releaseDate : 0,
         iconColor : "dark",
         imagePosition : {
             position: 'fixed',
@@ -27,4 +27,102 @@ export const songsArray = [
             left: "10vw"
         }
     },
+    {
+        name : "small, unnoticeable events",
+        fileName : "small-unnoticeable-events",
+        duration: 0,
+        backgroundColor : "#CAD3FA",
+        releaseDate : 0,
+        iconColor : "dark",
+        imagePosition : {
+            position: 'fixed',
+            width: '20vw',
+            top: "10vw",
+            left: "10vw"
+        }
+    },
+    {
+        name : "pressed up against a wall of teenage angst",
+        fileName : "pressed-up-against-a-wall-of-teenage-angst",
+        duration: 0,
+        backgroundColor : "#B4D9EB",
+        releaseDate : 0,
+        iconColor : "dark",
+        imagePosition : {
+            position: 'fixed',
+            width: '20vw',
+            top: "10vw",
+            left: "10vw"
+        }
+    },
+    {
+        name : "here is the world there is beyond",
+        fileName : "here-is-the-world-there-is-beyond",
+        duration: 0,
+        backgroundColor : "#1C3954",
+        releaseDate : 0,
+        iconColor : "light",
+        imagePosition : {
+            position: 'fixed',
+            width: '20vw',
+            top: "10vw",
+            left: "10vw"
+        }
+    },
+    {
+        name : "kept on pushing and pushing and pushing",
+        fileName : "kept-on-pushing-and-pushing-and-pushing",
+        duration: 0,
+        backgroundColor : "#023B58",
+        releaseDate : 0,
+        iconColor : "light",
+        imagePosition : {
+            position: 'fixed',
+            width: '20vw',
+            top: "10vw",
+            left: "10vw"
+        }
+    },
+    {
+        name : "as if it were some purified existence",
+        fileName : "as-if-it-were-some-purified-existence",
+        duration: 0,
+        backgroundColor : "#182354",
+        releaseDate : 0,
+        iconColor : "light",
+        imagePosition : {
+            position: 'fixed',
+            width: '20vw',
+            top: "10vw",
+            left: "10vw"
+        }
+    },
+    {
+        name : "no tricks, not yet",
+        fileName : "no-tricks-not-yet",
+        duration: 0,
+        backgroundColor : "#161730",
+        releaseDate : 0,
+        iconColor : "light",
+        imagePosition : {
+            position: 'fixed',
+            width: '20vw',
+            top: "10vw",
+            left: "10vw"
+        }
+    },
+    {
+        name : "angel, cut and cold",
+        fileName : "angel-cut-and-cold",
+        duration: 0,
+        backgroundColor : "#17253D",
+        releaseDate : 0,
+        iconColor : "light",
+        imagePosition : {
+            position: 'fixed',
+            width: '20vw',
+            top: "10vw",
+            left: "10vw"
+        }
+    }
 ]
