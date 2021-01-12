a69483ac69ff94021fa3718cabafd5aada4791d9
Isaac Poole <isaacpoolemusic@gmail.com>
2021-01-12 09:19:32 -0500

    cleanup

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -4,9 +4,6 @@ import ReactPlayer from 'react-player';
 export const  VideoPlayer = (props) => {
     const { source, isPlaying, setIsLoaded, next } = props;
 
-    const handleBuffer = () => {
-        console.log(`buffering`);
-    }
     return (
         <div className="player">
             <ReactPlayer 
@@ -14,7 +11,6 @@ export const  VideoPlayer = (props) => {
                 playing={isPlaying}
                 onReady={setIsLoaded}
                 onEnded={() => next()}
-                onBuffer={() => handleBuffer()}
                 width='100%'
                 height='100%'
             />
